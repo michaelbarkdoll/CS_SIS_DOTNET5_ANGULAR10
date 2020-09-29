f630421f410ec92fff638546e6acf8d45247f9dd
cisadmin <cisadmin@localhost.localdomain>
2020-09-29 11:39:54 -0500

    End of Section 3.

--- client/ssl/cmd.sh
+++ client/ssl/cmd.sh
@@ -1,3 +1,5 @@
 openssl req -newkey rsa:2048 -nodes -keyout privkey.pem -x509 -days 36500 -out certificate.pem -subj "/C=US/ST=NRW/L=Earth/O=CompanyName/OU=IT/CN=www.example.com/emailAddress=email@example.com"
+
+export NODE_OPTIONS="max_old_space_size=8096"
 ng serve --ssl --ssl-cert certificate.pem --ssl-key privkey.pem
 
